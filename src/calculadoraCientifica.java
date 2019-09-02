fc6da3a089fd812baa82f3f0b500b36e3ca63201
Jhonny Cataño <jhonnycatano@Jhonnys-MacBook-Pro.local>
2019-09-02 18:10:21 -0500

    this is going to be a cientific calculator that you are going to love

---
+++ src/calculadoraCientifica.java
@@ -0,0 +1,160 @@
+import.java.util.Scanner 
+public class calculadoracientifica{
+public static void main( string [] Args){
+	Scanner Reader = new Scanner (System.In);
+	Int basicOperation;
+	double number1;
+	double number2;
+	long pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091   
+    int base;
+    int exponent;
+
+           do {
+    	
+          ShowMenu();
+          basicOperation = Reader.nextInt();
+          ProcessOption(basicOperation);
+
+
+
+
+			public static int ShowMenu (int){
+			System.out.println = ("please select the number of the basic operation you would like to do: \n" +
+			"1. sum \n" +
+		    "2. subtration" +
+		    "3. division" +
+		    "4. multiplication" +
+		    "5. module" +
+		    "6. sinus" +
+		    "7. cosine" +
+		    "8. tangent" +
+		    "9. log " +
+		    "10. squareRoot" +
+		    "11. enesimaRoot" +
+		    "12. empowerment" +
+		    "13. base10OfAnumber" +
+		    "14. memoryOfOperaration" +);
+			}
+
+			public static double ProcessOption (int option){
+				switch(option){
+			 		case 1:
+					System.out.println ("please enter the number 1 that you would like to use");
+					number1= lector.Nextdouble();
+					System.out.println ("please enter the number 2 that you would like to use");
+					number2= lector.Nextdouble();
+					double total= sum(number1, number2);
+					break;
+
+					case 2:
+					System.out.println ("please enter the number 1 that you would like to use");
+					number1= lector.Nextdouble();
+					System.out.println ("please enter the number 2 that you would like to use");
+					number2= lector.Nextdouble();
+					double total= subtration(number1, number2);
+					break;
+
+					case 3:
+					System.out.println ("please enter the number 1 that you would like to use");
+					number1= lector.Nextdouble();
+					System.out.println ("please enter the number 2 that you would like to use");
+					number2= lector.Nextdouble();
+					double total= division(number1, number2);
+					break;
+
+					case 4:
+					System.out.println ("please enter the number 1 that you would like to use");
+					number1= lector.Nextdouble();
+					System.out.println ("please enter the number 2 that you would like to use");
+					number2= lector.Nextdouble();
+					double total= multiplication(number1, number2);
+					break;
+
+					case 5:
+					System.out.println ("please enter the number 1 that you would like to use");
+					number1= lector.Nextdouble();
+					System.out.println ("please enter the number 2 that you would like to use");
+					number2= lector.Nextdouble();
+					double total= module(number1, number2);
+					break;
+
+					case 6:
+					System.out.println ("please enter the number 1 that you would like to use");
+					number1= lector.Nextdouble();
+					System.out.println ("please enter the number 2 that you would like to use");
+					number2= lector.Nextdouble();
+					double total = sinus(number1, number2);
+					break;
+
+					case 12:
+					System.out.println ("please enter the exponent that you would like to use");
+					exponent= lector.Nextdouble();
+					System.out.println ("please enter the base that you would like to use");
+					base= lector.Nextdouble();
+
+
+
+					
+
+
+					default:
+					System.out.println ("error")
+
+				}
+				return total
+	        }
+
+
+
+
+			}
+			public static double sum(double n1, double n2){
+				return n1+n2;
+			}
+			public static double subtration(double n1, double n2){
+				return n1-n2;
+			}
+			public static double division(double n1, double n2){
+				return n1/n2
+			}
+			public static double multiplication(double n1, double n2){
+				return n1*n2
+			}
+			 public static double module(double n1, double n2){
+				return n1%n2
+			    }
+			 public static double sinus (double n1, double n2){
+			public static double angulosAradeanes(double angulos){
+				return (angulo*pi)/180;
+			public static double potencia(double base, int exp){
+					if(exp == 0 || base == 1){return 1;}
+					if(base == -1){return 1-(exp%2)*2;}
+
+			}
+
+
+	System.out.println = ("if you wish to use pi: 1.yes/, 2. no");
+	pi= lector.NextInt();
+	public static double sum
+
+
+
+	switch(basicOperation){
+	case 1:
+	()
+System.out.println ("would you like to do another operation? 1.yes/ 2.no")
+				operation2 = (sumOperation (basicOperation= reader.nextInt()
+
+
+
+
+
+
+	whatOperation = lector.nextInt()
+	System.out.Println = ("por favor ingresar el numero2 que desea utilizar");
+	System.Reader= number2
+
+	
+	
+
+    }}
